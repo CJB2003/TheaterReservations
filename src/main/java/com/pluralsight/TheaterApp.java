e390ea3423e59484efbc673126b0ab6306641d66
Chris Bunny <christopherjbunny@gmail.com>
2026-04-15 11:45:42 -0500

    starting on if statement and array

--- src/main/java/com/pluralsight/TheaterApp.java
+++ src/main/java/com/pluralsight/TheaterApp.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class TheaterApp {
+
+    public static Scanner myScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        //prompt user to enter name
+        System.out.print("Please enter your name: ");
+        String userName = myScanner.nextLine();
+        String[]
+
+        //prompt user to choose a date in specific format
+        System.out.print("What date will you be coming (MM/dd/yyyy): ");
+        String userDate = myScanner.nextLine();
+
+        //prompt user to input how many tickets they want
+        System.out.println("How many tickets would you like? ");
+        int userTicket = myScanner.nextInt();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate date = LocalDate.parse(userDate, formatter);
+
+        if (userTicket == 1) {
+            System.out.println(userTicket + " ticket reserved for " + date + " under " + );
+        }
+
+    }
 }
